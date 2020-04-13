dfab191c637fdfc725c8973d17bd1d385f368977
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-04-13 10:42:58 +0530

    Built web
    Branch: master
    Commit: ae89baba8612a1e8b035cb97f2d9fa756065bf1d
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,12 +2,13 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "d5219301e528f34d3aded3c3544b5ee9",
-"main.dart.js": "bfdcddf9032851be01221c5d4754be06",
+"main.dart.js": "48a3f6a3083f236a1f198371bd6b9998",
 "assets/LICENSE": "d04a91de2301fc16acc6e2c76139e25a",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/AssetManifest.json": "5a2fdb346a2ee8a2bc2390ca739d716d",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/countries.json": "ea6e9bd414ed0de188f3376a52ae60e3"
 };
 
 self.addEventListener('activate', function (event) {
