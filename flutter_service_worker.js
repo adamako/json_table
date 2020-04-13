943243733a00b3738fa713cc5d72afddccbeb890
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-04-13 09:58:29 +0530

    Built web
    Branch: master
    Commit: afc064f980cd38217a1f708202078595fa2d760b
    
    package:peanut 3.5.1

---
+++ flutter_service_worker.js
@@ -0,0 +1,35 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "d5219301e528f34d3aded3c3544b5ee9",
+"main.dart.js": "bfdcddf9032851be01221c5d4754be06",
+"assets/LICENSE": "d04a91de2301fc16acc6e2c76139e25a",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
